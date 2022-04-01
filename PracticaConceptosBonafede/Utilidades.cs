e5b4337f0acc5a9c1db324818ddabaeca420c533
MarcoBonafede <marcobonafede@hotmail.com>
2022-04-01 20:54:10 -0300

    Commit inicial y punto 1

---
+++ PracticaConceptosBonafede/Utilidades.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaConceptosBonafede
+{
+    public static class Utilidades
+    {
+        public static int ContarPalabras(this string input) // Punto 1
+        {
+            string[] palabras = input.Split(' ');
+            return palabras.Count();
+        }
+    }
+}
