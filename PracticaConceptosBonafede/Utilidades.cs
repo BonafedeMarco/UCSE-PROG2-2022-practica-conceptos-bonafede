390d2eaddf37f26093c113082b6588589190e52e
MarcoBonafede <marcobonafede@hotmail.com>
2022-04-06 15:52:24 -0300

    Commit hasta el punto 6 inclusive

--- PracticaConceptosBonafede/Utilidades.cs
+++ PracticaConceptosBonafede/Utilidades.cs
@@ -8,10 +8,70 @@ namespace PracticaConceptosBonafede
 {
     public static class Utilidades
     {
-        public static int ContarPalabras(this string input) // Punto 1
+        // Punto 1
+        public static int ContarPalabras(this string input)
         {
             string[] palabras = input.Split(' ');
             return palabras.Count();
+        }
+
+        // Punto 2
+        public static int ContarPalabras(this string input, char separador)
+        {
+            string[] palabras = input.Split(separador);
+            return palabras.Count();
+        }
+
+        // Punto 3
+        public static decimal CalcularDiferenciaEnMinutos(this DateTime fechaA, DateTime fechaB)
+        {
+            return Convert.ToDecimal(Math.Sqrt((fechaA.Second - fechaB.Second)^2)) * 60;
+        }
+
+        // Punto 4
+        public static double ObtenerPromedio(this List<int> listaEnteros)
+        {
+            return listaEnteros.Average();
+        }
+
+        private static bool EsPar(this int numero)
+        {
+            if ((numero / 2) == (Convert.ToDouble(numero) / 2))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // Punto 5
+        public static int ObtenerSumaParesPorRestaImpares(this List<int> listaEnteros)
+        {
+            int sumaPares = 0;
+            int restaImpares = 0;
+            foreach (int numero in listaEnteros)
+            {
+                if (numero.EsPar())
+                    sumaPares += numero;
+                else
+                    restaImpares -= numero;
+            }
+            return sumaPares * restaImpares;
+        }
+
+        // Punto 6
+        public static DateTime ConvertirStringAFecha(this string input)
+        {
+            DateTime fecha;
+            try
+            {
+                string[] valores = input.Split('/');
+                fecha = new DateTime(Convert.ToInt32(valores[2]), Convert.ToInt32(valores[1]), Convert.ToInt32(valores[0]));
+            }
+            catch (Exception)
+            {
+                fecha = DateTime.MinValue;
+            }
+            return fecha;
         }
     }
 }
